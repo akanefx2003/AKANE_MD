89d3d12d12533a992bd410cde58404fb8f706d19
akanefx2003 <akanefx1@gmail.com>
2026-04-09 09:08:23 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-module.exports = {
-  Number: "221705928204" // remplacez par votre numéro WhatsApp sans mettre le signe +
+export default {
+    Number: "221705928204" // remplacez par votre numéro WhatsApp sans mettre le signe +
 };
