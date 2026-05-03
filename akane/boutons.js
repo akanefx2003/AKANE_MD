2913cf3ec7ccfe1c241664463e1e8031e1907e67
akanefx2003 <akanefx1@gmail.com>
2026-05-03 00:51:24 +0000

    Add files via upload

---
+++ akane/boutons.js
@@ -0,0 +1,11 @@
+const canalInfo = {
+    isForwarded: true,
+    forwardingScore: 1,
+    forwardedNewsletterMessageInfo: {
+        newsletterJid: "120363423070848478@newsletter",
+        serverMessageId: 100,
+        newsletterName: "Voir la chaîne officielle 📢"
+    }
+};
+
+export { canalInfo }
