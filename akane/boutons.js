08ecfed47cbe151ee200bb7402e299b1e6983fc5
akanefx2003 <akanefx1@gmail.com>
2026-06-15 22:24:52 +0000

    Update boutons.js

--- akane/boutons.js
+++ akane/boutons.js
@@ -2,7 +2,7 @@ const canalInfo = {
     isForwarded: true,
     forwardingScore: 1,
     forwardedNewsletterMessageInfo: {
-        newsletterJid: "120363423070848478@newsletter",
+        newsletterJid: "120363428381322603@newsletter",
         serverMessageId: 100,
         newsletterName: "Voir la chaîne officielle 📢"
     }
