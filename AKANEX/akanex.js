464a2903fddf7fa721549e80f0733e60e4bc8d6f
akanefx2003 <akanefx1@gmail.com>
2026-09-05 19:15:24 +0000

    Update akanex.js

--- AKANEX/akanex.js
+++ AKANEX/akanex.js
@@ -1,4 +1,4 @@
-import { makeWASocket, useMultiFileAuthState, DisconnectReason, fetchLatestBaileysVersion } from '@crysnovax/baileys';
+import { makeWASocket, useMultiFileAuthState, DisconnectReason, fetchLatestBaileysVersion } from 'crysnovax/baileys';
 import pino from 'pino';
 import fs from 'fs';
 import path from 'path';
