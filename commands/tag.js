24f7ae45e933bab73e9e8a40549f34012553f625
akanefx2003 <akanefx1@gmail.com>
2026-09-08 00:24:20 +0000

    Update tag.js

--- commands/tag.js
+++ commands/tag.js
@@ -2,7 +2,7 @@
 // @cat: gc-menu
 
 import fs from 'fs'
-import { downloadMediaMessage } from '@crysnovax/baileys'
+import { downloadMediaMessage } from'@whiskeysockets/baileys',
 
 const ANTITAG_FILE = './data/antitag.json'
 
